b7ade460a3fc02fdfcc55aa329c7dc673046defa
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-13 10:51:48 -0500

    finished express server unit

--- express-js/3.1 Express Server/index.js
+++ express-js/3.1 Express Server/index.js
@@ -5,3 +5,4 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+// sudo lsof -i -P -n | grep LISTEN  = this looks at all the ports being occupied
