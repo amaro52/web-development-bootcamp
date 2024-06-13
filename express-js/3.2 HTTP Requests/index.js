ed5ce3dff1b68092ad0032d710770666d1c91551
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-13 11:09:06 -0500

    finished 3.2 HTTP requests

--- express-js/3.2 HTTP Requests/index.js
+++ express-js/3.2 HTTP Requests/index.js
@@ -3,15 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  // console.log(req.rawHeaders);
   res.send("<h1>Hello</h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+  res.send("<h1>About Me</h1><p>My name is Amogh</p>");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+  res.send("<h1>Contact Me</h1><p>Phone: +1 (xxx) xxx-xxxx</p>");
 });
 
 app.listen(port, () => {
