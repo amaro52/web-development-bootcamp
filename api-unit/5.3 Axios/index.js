88cbe821f2af3ddff0d68fcc4da61afcddb40992
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-15 19:01:30 -0500

    finished Axios API exercise

--- api-unit/5.3 Axios/index.js
+++ api-unit/5.3 Axios/index.js
@@ -25,8 +25,27 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  try {
+    // console.log(req.body);
 
+    let type = req.body.type;
+    let numOfParticipants = req.body.participants;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${numOfParticipants}`
+    );
+
+    let result = response.data;
+    // console.log(result);
+
+    res.render("index.ejs", {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
