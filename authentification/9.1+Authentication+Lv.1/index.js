b6d56719ca5f8317a43e3ba031bc4439784779cd
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-23 10:33:48 -0500

    added .gitignore

--- authentification/9.1+Authentication+Lv.1/index.js
+++ authentification/9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import config from "./config.js";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: config.PASSWORD,
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,22 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  // put username and password into database
+
+  res.redirect("/"); // send back to home page so they log in
+});
+
+app.post("/login", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  // check that username and password are in database
+  res.redirect("/"); // send back to home page so they log in
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
